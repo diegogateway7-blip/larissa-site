892c5b84014a74f66eedb2eab79fcd1fa893fca9
diego gateway <diegogateway7@gmail.com>
2025-10-30 00:57:36 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@ import { ProfileCard } from '@/components/content-cove/profile-card';
 import { ExclusiveContent } from '@/components/content-cove/exclusive-content';
 import { Button } from '@/components/ui/button';
 import { Send } from 'lucide-react';
-import { ProfileHeader } from '@/components/content-cove/profile-header';
 
 export default function Home() {
   return (
     <div className="bg-card min-h-screen font-body">
-      <ProfileHeader />
       <main className="container mx-auto max-w-4xl px-0 sm:px-4">
         <ProfileCard />
         <div className="px-4">
